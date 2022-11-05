664d091e53d80d010295abb07a11d859c7eddc89
Jordin Kolman <jordinkolman@gmail.com>
2022-11-05 09:43:50 -0500

    created simple grading app

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Example2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double score = 0.0;
+            Console.Write("Enter a score: ");
+            score = Convert.ToDouble(Console.ReadLine());
+            if (score >= 80.0)
+                Console.WriteLine("You earned an A!");
+            else if (score >= 80.0)
+                Console.WriteLine("You earned a B");
+            else if (score >= 70.0)
+                Console.WriteLine("You earned a C");
+            else if (score >= 60.0)
+                Console.WriteLine("You earned a D");
+            else
+                Console.WriteLine("Why did you get an F?");            
+        }
+    }
+}
